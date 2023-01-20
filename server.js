e1b8c2b599a7f51c72cb86f3b51206acdace2952
Maddie Gillis <madelinegillis@outlook.com>
2023-01-20 13:45:34 -0800

    Added initial requirements needed for express to server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const express = require('express.js')
+//Brings in express and fs
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+//sets port for Heroku or 3001
+const PORT = process.env.PORT || 3001
+
+//Sets express to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
