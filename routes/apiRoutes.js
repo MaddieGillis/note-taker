314306b4c90f54c18a12abeec6051c902ee50e04
MaddieGillis <112782694+MaddieGillis@users.noreply.github.com>
2023-02-12 17:42:00 -0800

    Fixed error in apiRoutes so now it's router.get instead of app

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,40 @@
 const fs = require('fs');
 const util = require('util');
-const app = require('express').Router();
+const app = require('express');
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 const router = require('express').Router();
+
 let notesInfo;
 
+
+
 //GET
-app.get("/notes", (req, res) => {
-    readFileAsync('db/db.json', 'utf8').then(function (data) {
+router.get("/notes", (req, res) => {
+    readFileAsync('db/db.json').then(function (data) {
         notesInfo = JSON.parse(data);
-        res.json(notesData)
+        res.json(notesInfo);
     })
 });
 
 //POST
 
-app.post('/notes', (req, res) => {
-    readFileAsync("db/db.json", "uft8").then(function (data) {
+router.post('/notes', (req, res) => {
+    readFileAsync("db/db.json").then(function (data) {
         notesInfo = JSON.parse(data);
 
         let newPost = req.body;
-        let currentId = notesData.length;
+        let currentId = notesInfo.length;
 
         newPost.id = currentId + 1;
 
         notesInfo.push(newPost);
 
-        notesInfo = JSON.stringify(notesData);
+        notesInfo = JSON.stringify(notesInfo);
 
-        writeFileAsync('db/db.json', notesInfo);
+        writeFileAsync('db/db.json', notesInfo).then(function (data) {
+            console.log("Added note");
+        });
 
         res.json(notesInfo);
     });
